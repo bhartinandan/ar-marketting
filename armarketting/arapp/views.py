4b05138ff1ee2620ba13f4893cfe4de260a237af
Bharti Nandan <53074374+bhartinandan@users.noreply.github.com>
2026-02-15 22:17:30 +0530

    view update

--- armarketting/arapp/views.py
+++ armarketting/arapp/views.py
@@ -1119,7 +1119,7 @@ def play_ar_game(request, hashid):
         userid = decode_primary_key(hashid)
         print("userid", userid)
         argame = ArGame.objects.filter(id=userid).first()
-        return render(request, "game/play_game.html", context={
+        return render(request, "game/game_page.html", context={
             "max_size": argame.max_size,
             "min_size": argame.min_size,
             "min_distance": argame.min_distance,
